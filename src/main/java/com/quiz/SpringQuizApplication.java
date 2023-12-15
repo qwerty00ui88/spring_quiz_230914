f025ea8c47e7c5907c22d205234ba89d9b0cb6f3
test <test@test.com>
2023-12-15 18:49:13 +0900

    lesson01-quiz01

--- src/main/java/com/quiz/SpringQuizApplication.java
+++ src/main/java/com/quiz/SpringQuizApplication.java
@@ -1,12 +1,8 @@
 package com.quiz;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// 설정 파일
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class SpringQuizApplication {
 
