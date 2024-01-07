95f06082b61e65e40376478fde64607ba2d192ab
hameee <sohui.dev@gmail.com>
2024-01-07 21:44:55 +0900

    lesson07-quiz01

--- src/main/java/com/quiz/lesson07/Lesson07Quiz01RestController.java
+++ src/main/java/com/quiz/lesson07/Lesson07Quiz01RestController.java
@@ -1,16 +1,50 @@
 package com.quiz.lesson07;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quiz.lesson07.bo.CompanyBO;
+import com.quiz.lesson07.entiry.CompanyEntity;
 
-@RequestMapping("/lesson07")
+@RequestMapping("/lesson07/quiz01")
 @RestController
 public class Lesson07Quiz01RestController {
 	
 	@Autowired 
 	private CompanyBO companyBO;
+	
+	// http://localhost/lesson07/quiz01/save1
+	@GetMapping("/save1")
+	public String create1() {
+		String name = "넥손";
+		String business = "컨텐츠 게임";
+		String scale = "대기업";
+		int headcount = 3585;
+		
+		CompanyEntity company = companyBO.addCompany(name, business, scale, headcount);
+		
+		return "<div>회사명 : " + company.getName() + "</div>"
+				+ "<div>사업내용 : " + company.getBusiness() + "</div>"
+				+ "<div>규모 : " + company.getScale() + "</div>"
+				+ "<div>사원수 : " + company.getHeadcount() + "명</div>";
+	}
+	
+	// http://localhost/lesson07/quiz01/save2
+	@GetMapping("/save2")
+	public String create2() {
+		String name = "버블팡";
+		String business = "여신 금융업";
+		String scale = "대기업";
+		int headcount = 6834;
+		
+		CompanyEntity company = companyBO.addCompany(name, business, scale, headcount);
+		
+		return "<div>회사명 : " + company.getName() + "</div>"
+		+ "<div>사업내용 : " + company.getBusiness() + "</div>"
+		+ "<div>규모 : " + company.getScale() + "</div>"
+		+ "<div>사원수 : " + company.getHeadcount() + "명</div>";
+	}
 	
 }
--- src/main/java/com/quiz/lesson07/bo/CompanyBO.java
+++ src/main/java/com/quiz/lesson07/bo/CompanyBO.java
@@ -3,6 +3,7 @@ package com.quiz.lesson07.bo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.quiz.lesson07.entiry.CompanyEntity;
 import com.quiz.lesson07.repository.CompanyRepository;
 
 @Service
@@ -10,5 +11,19 @@ public class CompanyBO {
 	
 	@Autowired
 	private CompanyRepository companyRepository;
+	
+	public CompanyEntity addCompany(String name, String business, String scale, int headcount) {
+		
+		// CompanyEntity 생성
+		CompanyEntity company = CompanyEntity.builder()
+				.name(name)
+				.business(business)
+				.scale(scale)
+				.headcount(headcount)
+				.build();
+				
+		// Repository(Spring Data JPA)로 넘겨주기 + return
+		return companyRepository.save(company);
+	}
 	
 }
